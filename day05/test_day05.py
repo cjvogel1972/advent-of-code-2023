a315e199a85563341eef35d84a94907f37d84b90
Chris Vogel <cjvogel1972@gmail.com>
2023-12-06 20:16:43 -0600

    Code cleanup for Day 5
    Greatly simplified the parsing of the file. Broke the file up into
    blocks, rather than the way I looked for blank lines in my loop.
    Also took more advantage of split on the strings. And learned about
    the map function (a bit nicer than list comprehension for this case).
    Because of switching to reading blocks, I put the example input in
    a file to properly parse the input into blocks.
    
    Switched from maps to parallel lists for the seeds and resulting
    values. It just became easier, rather than trying to initialize the
    map values to sys.maxsize. This also made it easier to use the min()
    function with part 2.
    
    Didn't like "inside", because it wasn't necessarily inside anything,
    but overlap from the two ranges. So, switched it to "overlap".
    
    Instead of using the size and doing math with the Mapping and
    SeedRange, I just did it in the class and used a proprty for the end.
    
    Added a method on Mapping to map a value to a destination, just to
    have the logic in one place.
    
    Took inspiration for file parsing from HyperNeutrino:
    https://youtu.be/NmxHw_bHhGM?si=tQ2Pp8ugQqk00xXt

--- day05/test_day05.py
+++ day05/test_day05.py
@@ -1,52 +1,16 @@
 import unittest
 
 import day05 as day
+from util.file import readfile_blocks
 
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
-        self.lines = [
-            "seeds: 79 14 55 13",
-            "",
-            "seed-to-soil map:",
-            "50 98 2",
-            "52 50 48",
-            "",
-            "soil-to-fertilizer map:",
-            "0 15 37",
-            "37 52 2",
-            "39 0 15",
-            "",
-            "fertilizer-to-water map:",
-            "49 53 8",
-            "0 11 42",
-            "42 0 7",
-            "57 7 4",
-            "",
-            "water-to-light map:",
-            "88 18 7",
-            "18 25 70",
-            "",
-            "light-to-temperature map:",
-            "45 77 23",
-            "81 45 19",
-            "68 64 13",
-            "",
-            "temperature-to-humidity map:",
-            "0 69 1",
-            "1 0 69",
-            "",
-            "humidity-to-location map:",
-            "60 56 37",
-            "56 93 4"
-        ]
-
     def test_solve_part1(self):
-        self.assertEqual(35, day.solve_part1(self.lines))
+        self.assertEqual(35, day.solve_part1(readfile_blocks("input-ex.txt")))
 
     def test_solve_part2(self):
-        self.assertEqual(46, day.solve_part2(self.lines))
+        self.assertEqual(46, day.solve_part2(readfile_blocks("input-ex.txt")))
 
 
 if __name__ == '__main__':
--- util/file.py
+++ util/file.py
@@ -6,3 +6,11 @@ def readfile(filename: str) -> list[str]:
     lines = [line.strip() for line in lines]
 
     return lines
+
+
+def readfile_blocks(filename: str) -> list[str]:
+    """Return blocks of data from the file as a list, based on there being blank lines separating the blocks"""
+    with open(filename) as f:
+        blocks = f.read().split("\n\n")
+
+    return blocks
