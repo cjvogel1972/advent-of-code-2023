83b546ebd663155ae99c017a9fc84dc09dba0b2f
Chris Vogel <cjvogel1972@gmail.com>
2023-12-08 09:21:29 -0600

    Code cleanup for Day 8
    Refactored common code into their own functions.

--- day08/day08.py
+++ day08/day08.py
@@ -1,20 +1,56 @@
+import re
 from math import lcm
 
 from util.file import readfile
-import re
 
 node_re = re.compile("([A-Z0-9]+) = \\(([A-Z0-9]+), ([A-Z0-9]+)\\)")
 
 
 def solve_part1(lines: list[str]) -> int:
+    directions, nodes = parse_directions_nodes(lines)
+
+    curr_node = nodes['AAA']
+    moves = compute_moves(curr_node, directions, nodes, part1_at_end_check)
+
+    return moves
+
+
+def part1_at_end_check(name: str) -> bool:
+    return name == 'ZZZ'
+
+
+def solve_part2(lines: list[str]) -> int:
+    directions, nodes = parse_directions_nodes(lines)
+
+    curr_nodes = []
+    for name in nodes.keys():
+        if name[-1] == 'A':
+            curr_nodes.append(nodes[name])
+
+    all_moves = []
+    for curr_node in curr_nodes:
+        moves = compute_moves(curr_node, directions, nodes, part2_at_end_check)
+        all_moves.append(moves)
+
+    return lcm(*all_moves)
+
+
+def part2_at_end_check(name: str) -> bool:
+    return name[-1] == 'Z'
+
+
+def parse_directions_nodes(lines):
     directions = lines[0]
     nodes = {}
     for i in range(2, len(lines)):
         node = Node(lines[i])
         nodes[node.name] = node
 
+    return directions, nodes
+
+
+def compute_moves(curr_node, directions, nodes, end_check):
     at_end = False
-    curr_node = nodes['AAA']
     moves = 0
     while not at_end:
         for d in directions:
@@ -22,42 +58,14 @@ def solve_part1(lines: list[str]) -> int:
                 curr_node = nodes[curr_node.left]
             else:
                 curr_node = nodes[curr_node.right]
+
             moves += 1
-            if curr_node.name == 'ZZZ':
+
+            if end_check(curr_node.name):
                 at_end = True
                 break
 
     return moves
-
-
-def solve_part2(lines: list[str]) -> int:
-    directions = lines[0]
-    nodes = {}
-    for i in range(2, len(lines)):
-        node = Node(lines[i])
-        nodes[node.name] = node
-
-    curr_nodes = []
-    for name in nodes.keys():
-        if name[-1] == 'A':
-            curr_nodes.append(nodes[name])
-    all_moves = []
-    for curr_node in curr_nodes:
-        moves = 0
-        at_end = False
-        while not at_end:
-            for d in directions:
-                if d == 'L':
-                    curr_node = nodes[curr_node.left]
-                else:
-                    curr_node = nodes[curr_node.right]
-                moves += 1
-                if curr_node.name[-1] == 'Z':
-                    at_end = True
-                    break
-        all_moves.append(moves)
-
-    return lcm(*all_moves)
 
 
 class Node:
