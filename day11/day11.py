6eab9c30e49957cca413680dd3e6e7cb31c0f033
Chris Vogel <cjvogel1972@gmail.com>
2023-12-11 11:55:16 -0500

    Code cleanup for Day 11
    Came up with a MUCH better way to process everything. Rather than
    doing everything by brute force, keep track of where the galaxies
    are and when we expand the universe, modify the coordinates of
    the galaxy. One key thing is to look at the universe expansion in
    reverse order, because you could have a galaxy move one too many
    times. Ultimately, I'm able to use the same function for both parts,
    with passing in the size of the expansion.

--- day11/day11.py
+++ day11/day11.py
@@ -1,110 +1,54 @@
 from util.file import readfile
 
 
-def solve_part1(lines: list[str]) -> int:
-    total = 0
+def solve(lines: list[str], expansion: int) -> int:
+    galaxies = find_galaxies(lines)
+    expand_universe(galaxies, lines, expansion)
+    return compute_total_shortest_distance(galaxies)
 
-    skip_next = False
-    for r, row in enumerate(lines):
-        if skip_next:
-            skip_next = False
-            continue
-        if row.count('.') == len(row):
-            lines.insert(r, row)
-            skip_next = True
 
-    cols = zip(*lines)
-    col_inserted = 0
-    for c, col in enumerate(cols):
-        if col.count('.') == len(col):
-            for i, line in enumerate(lines):
-                lines[i] = line[:c + col_inserted] + '.' + line[c + col_inserted:]
-            col_inserted += 1
+def find_galaxies(lines):
+    galaxies = []
 
     for r, row in enumerate(lines):
         start_loc = 0
         while row.count('#', start_loc) > 0:
             c = row.index("#", start_loc)
+            galaxies.append((r, c))
             start_loc = c + 1
-
-            next_start_loc = c + 1
-            while row.count("#", next_start_loc) > 0:
-                c1 = row.index("#", next_start_loc)
-                next_start_loc = c1 + 1
-                total += (c1 - c)
-
-            for r1 in range(r + 1, len(lines)):
-                row1 = lines[r1]
-                next_start_loc = 0
-                while row1.count("#", next_start_loc) > 0:
-                    c1 = row1.index("#", next_start_loc)
-                    next_start_loc = c1 + 1
-                    total += abs(r1 - r) + abs(c1 - c)
-
-    return total
+            
+    return galaxies
 
 
-def solve_part2(lines: list[str]) -> int:
-    expansion = 1000000
+def expand_universe(galaxies, lines, expansion):
+    for r, row in reversed(list(enumerate(lines))):
+        if row.count('.') == len(row):
+            for g, galaxy in enumerate(galaxies):
+                if galaxy[0] > r:
+                    galaxies[g] = (galaxy[0] + expansion - 1, galaxy[1])
+
+    cols = zip(*lines)
+    for c, col in reversed(list(enumerate(cols))):
+        if col.count('.') == len(col):
+            for g, galaxy in enumerate(galaxies):
+                if galaxy[1] > c:
+                    galaxies[g] = (galaxy[0], galaxy[1] + expansion - 1)
+
+
+def compute_total_shortest_distance(galaxies) -> int:
     total = 0
 
-    expansion_str = '.' * expansion
-    cols = zip(*lines)
-    col_inserted = 0
-    for c, col in enumerate(cols):
-        if col.count('.') == len(col):
-            for i, line in enumerate(lines):
-                lines[i] = line[:c + col_inserted] + expansion_str + line[c + 1 + col_inserted:]
-            col_inserted += expansion - 1
-
-    empty_rows = []
-    for r, row in enumerate(lines):
-        if row.count('.') == len(row):
-            empty_rows.append(r)
-
-    expansion_lines = [lines[empty_rows[0]]] * expansion
-    for i in reversed(empty_rows):
-        lines[i:i + 1] = expansion_lines
-
-    r = 0
-    while r < len(lines):
-        print(r)
-        row = lines[r]
-        if row.count('.') == len(row):
-            r += expansion
-            continue
-        start_loc = 0
-        while row.count('#', start_loc) > 0:
-            c = row.index("#", start_loc)
-            start_loc = c + 1
-
-            next_start_loc = c + 1
-            while row.count("#", next_start_loc) > 0:
-                c1 = row.index("#", next_start_loc)
-                next_start_loc = c1 + 1
-                total += (c1 - c)
-
-            r1 = r + 1
-            while r1 < len(lines):
-                row1 = lines[r1]
-                if row1.count('.') == len(row1):
-                    r1 += expansion
-                    # print(r1)
-                    continue
-                next_start_loc = 0
-                while row1.count("#", next_start_loc) > 0:
-                    c1 = row1.index("#", next_start_loc)
-                    next_start_loc = c1 + 1
-                    total += abs(r1 - r) + abs(c1 - c)
-                r1 += 1
-        r += 1
+    for i in range(len(galaxies) - 1):
+        g1 = galaxies[i]
+        for j in range(i + 1, len(galaxies)):
+            g2 = galaxies[j]
+            total += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
 
     return total
 
 
 if __name__ == '__main__':
     input_lines = readfile("day11/input.txt")
-    print(f'Part 1: {solve_part1(input_lines)}')
 
-    input_lines = readfile("day11/input.txt")
-    print(f'Part 2: {solve_part2(input_lines)}')
+    print(f'Part 1: {solve(input_lines, 2)}')
+    print(f'Part 2: {solve(input_lines, 1000000)}')
