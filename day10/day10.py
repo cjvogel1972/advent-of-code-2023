429d1550798c6094a07e915ea9e41e715d29fe77
Chris Vogel <cjvogel1972@gmail.com>
2023-12-11 00:06:48 -0500

    Solution for Day 10 Part 1
    Part 1 took some time, but I actually got it. Part 2 has been hell
    trying to figure out. I'm guessing that I'm doing things wrong and
    it would be a lot easier if I did it a different way.
    
    I did find out that I need to use "point in polygon", but need to
    figure out the type of pipe the start is.
    
    So, I'm committing part 1 for now and will continue to work on part
    2.
    
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     10   01:54:27   7524      0          -      -      -

---
+++ day10/day10.py
@@ -0,0 +1,132 @@
+from util.file import readfile
+
+
+def solve_part1(lines: list[str]) -> int:
+    grid, distance = create_grids(lines)
+    start = find_start(grid)
+
+    compute_pipe_distances(grid, distance, start)
+
+    return max([max(row) for row in distance])
+
+
+def create_grids(lines):
+    grid = [list(line) for line in lines]
+    distance = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    return grid, distance
+
+
+def find_start(grid: list[list[str]]) -> tuple:
+    for r, row in enumerate(grid):
+        if row.count('S') == 1:
+            return r, row.index('S')
+
+    raise ValueError("Could not find the start of the pipes")
+
+
+def compute_pipe_distances(grid, distance, start):
+    steps = compute_next_steps(grid, *start)
+    while steps:
+        current = steps.pop()
+        new_step = follow_pipe(grid, distance, *current)
+        if new_step[1] != (0, 0):
+            steps.insert(0, new_step)
+
+
+def compute_next_steps(grid: list[list[str]], r: int, c: int) -> list[tuple]:
+    location = (r, c)
+    steps = []
+
+    if is_location_in_bounds(grid, r - 1, c):
+        steps.append((location, (-1, 0)))
+    if is_location_in_bounds(grid, r + 1, c):
+        steps.append((location, (1, 0)))
+    if is_location_in_bounds(grid, r, c - 1):
+        steps.append((location, (0, -1)))
+    if is_location_in_bounds(grid, r, c + 1):
+        steps.append((location, (0, 1)))
+
+    return steps
+
+
+def follow_pipe(grid: list[list[str]], distance: list[list[int]], location: tuple, direction: tuple) -> tuple:
+    r, c = location
+    dir_r, dir_c = direction
+    dist = distance[r][c]
+    new_r = r + dir_r
+    new_c = c + dir_c
+
+    if not is_location_in_bounds(grid, new_r, new_c):
+        return (0, 0), (0, 0)
+
+    ch = grid[new_r][new_c]
+    if ch == '.':
+        return (0, 0), (0, 0)
+
+    if distance[new_r][new_c] != 0:
+        if distance[new_r][new_c] + 1 < dist:
+            distance[new_r][new_c] = dist + 1
+        return (0, 0), (0, 0)
+
+    if dir_r != 0:
+        if ch in '|FLJ7':
+            distance[new_r][new_c] = dist + 1
+        if ch == '|':
+            return (new_r, new_c), direction
+        if ch in 'FL':
+            return (new_r, new_c), (0, 1)
+        if ch in 'J7':
+            return (new_r, new_c), (0, -1)
+
+    if dir_c != 0:
+        if ch in ('-', 'F', '7', 'J', 'L'):
+            distance[new_r][new_c] = dist + 1
+        if ch == '-':
+            return (new_r, new_c), direction
+        if ch in ('F', '7'):
+            return (new_r, new_c), (1, 0)
+        if ch in ('J', 'L'):
+            return (new_r, new_c), (-1, 0)
+
+    return (0, 0), (0, 0)
+
+
+def is_location_in_bounds(grid: list[list[str]], r: int, c: int) -> bool:
+    return 0 <= r < len(grid) and 0 <= c < len(grid[0])
+
+
+def solve_part2(lines: list[str]) -> int:
+    grid, distance = create_grids(lines)
+    start = find_start(grid)
+
+    compute_pipe_distances(grid, distance, start)
+
+    # get rid of unnecessary pipes
+    for r, row in enumerate(distance):
+        for c, d in enumerate(row):
+            if d == 0 and grid[r][c] != 'S':
+                grid[r][c] = '.'
+    grid[start[0]][start[1]] = '-'
+
+    for r, row in enumerate(grid):
+        outside = True
+        for c, ch in enumerate(row):
+            if ch == '.' and outside:
+                grid[r][c] = 'O'
+            elif ch == '.' and not outside:
+                grid[r][c] = 'I'
+            if ch in '|LJ':
+                outside = not outside
+
+    total = 0
+    for row in grid:
+        total += row.count('I')
+
+    return total
+
+
+if __name__ == '__main__':
+    input_lines = readfile("day10/input.txt")
+
+    print(f'Part 1: {solve_part1(input_lines)}')
+    print(f'Part 2: {solve_part2(input_lines)}')
