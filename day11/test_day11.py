d66b01436a58ee621a56ab3c9adab1f7c70fc089
Chris Vogel <cjvogel1972@gmail.com>
2023-12-11 11:55:16 -0500

    Code cleanup for Day 11
    Came up with a MUCH better way to process everything. Rather than
    doing everything by brute force, keep track of where the galaxies
    are and when we expand the universe, modify the coordinates of
    the galaxy. One key thing is to look at the universe expansion in
    reverse order, because you could have a galaxy move one too many
    times. Ultimately, I'm able to use the same function for both parts,
    with passing in the size of the expansion.

--- day11/test_day11.py
+++ day11/test_day11.py
@@ -20,10 +20,11 @@ class Test(unittest.TestCase):
         ]
 
     def test_solve_part1(self):
-        self.assertEqual(374, day.solve_part1(self.lines))
+        self.assertEqual(374, day.solve(self.lines, 2))
 
     def test_solve_part2(self):
-        self.assertEqual(8410, day.solve_part2(self.lines))
+        self.assertEqual(1030, day.solve(self.lines, 10))
+        self.assertEqual(8410, day.solve(self.lines, 100))
 
 
 if __name__ == '__main__':
