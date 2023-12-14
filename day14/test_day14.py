d6a2f1d040cb13cd1a50e5803ebaf31de5356c66
Chris Vogel <cjvogel1972@gmail.com>
2023-12-14 13:59:25 -0500

    Solution for Day 14
    Part 2 was a little more difficult. Obviously brute force of 1
    billion would not be right. Thanks to Scott for cluing me in that
    there was a loop of positions. Took a little bit to make a generic
    tilt capability. Then it took a bit to get my math right for the
    delta that I needed to do more cycles.
    
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     14   07:23:48  18822      0   13:47:41  17979      0

---
+++ day14/test_day14.py
@@ -0,0 +1,35 @@
+import unittest
+
+import day14 as day
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.lines = [
+            "O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#...."
+        ]
+
+    def test_solve_part1(self):
+        self.assertEqual(136, day.solve_part1(self.lines))
+
+    def test_solve_part2(self):
+        self.assertEqual(64, day.solve_part2(self.lines))
+
+    def test_cycle(self):
+        grid = [list(line) for line in self.lines]
+        expected = ".....#........#...O#...OO##....OO#...........OOO#..O#...O#.#....O#..........OOOO#...O###..#..OO#...."
+        day.cycle(grid)
+        self.assertEqual(expected, day.platform_to_string(grid))
+
+if __name__ == '__main__':
+    unittest.main()
