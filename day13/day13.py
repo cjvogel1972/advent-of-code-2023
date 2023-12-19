55f9d590bf4fe1ee77b93f2f9218b4e8721f8624
Chris Vogel <cjvogel1972@gmail.com>
2023-12-18 21:43:58 -0600

    Code cleanup for Day 13
    Consolidate the duplicate code.

--- day13/day13.py
+++ day13/day13.py
@@ -2,32 +2,40 @@ from util.file import readfile_blocks
 
 
 def solve_part1(blocks: list[str]) -> int:
+    return summarize_reflections(blocks, check_mirroring)
+
+
+def summarize_reflections(blocks, check):
     total = 0
 
     for block in blocks:
         lines = block.splitlines()
 
-        mirror_start = -1
-        for i in range(len(lines) - 1):
-            if check_mirroring(lines, i):
-                mirror_start = i + 1
-                break
+        line_of_reflection = find_line_of_reflection(lines, check)
 
-        if mirror_start != -1:
-            total += mirror_start * 100
+        if line_of_reflection != -1:
+            total += line_of_reflection * 100
             continue
 
         vertical = list(map("".join, zip(*lines)))
-        for i in range(len(vertical) - 1):
-            if check_mirroring(vertical, i):
-                mirror_start = i + 1
-                break
+        line_of_reflection = find_line_of_reflection(vertical, check)
 
-        if mirror_start != -1:
-            total += mirror_start
+        if line_of_reflection != -1:
+            total += line_of_reflection
             continue
 
     return total
+
+
+def find_line_of_reflection(lines: list[str], check):
+    line_of_reflection = -1
+
+    for i in range(len(lines) - 1):
+        if check(lines, i):
+            line_of_reflection = i + 1
+            break
+
+    return line_of_reflection
 
 
 def check_mirroring(lines: list[str], mirror_start: int) -> bool:
@@ -44,32 +52,7 @@ def check_mirroring(lines: list[str], mirror_start: int) -> bool:
 
 
 def solve_part2(blocks: list[str]) -> int:
-    total = 0
-
-    for block in blocks:
-        lines = block.splitlines()
-
-        mirror_start = -1
-        for i in range(len(lines) - 1):
-            if check_mirroring_smudge(lines, i):
-                mirror_start = i
-                break
-
-        if mirror_start != -1:
-            total += (mirror_start + 1) * 100
-            continue
-
-        vertical = list(map("".join, zip(*lines)))
-        for i in range(len(vertical) - 1):
-            if check_mirroring_smudge(vertical, i):
-                mirror_start = i
-                break
-
-        if mirror_start != -1:
-            total += mirror_start + 1
-            continue
-
-    return total
+    return summarize_reflections(blocks, check_mirroring_smudge)
 
 
 def check_mirroring_smudge(lines: list[str], mirror_start: int) -> bool:
@@ -87,12 +70,7 @@ def check_mirroring_smudge(lines: list[str], mirror_start: int) -> bool:
 
 
 def count_diffs(line1: str, line2: str) -> int:
-    count = 0
-    for c1, c2 in zip(line1, line2):
-        if c1 != c2:
-            count += 1
-
-    return count
+    return sum([1 if c1 != c2 else 0 for c1, c2 in zip(line1, line2)])
 
 
 if __name__ == '__main__':
