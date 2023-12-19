079bd55c3df7957999c46be4d58a629f3a53f095
Chris Vogel <cjvogel1972@gmail.com>
2023-12-18 22:38:34 -0600

    Code cleanup for Day 15
    Just some simplification of code, list comprehension and sequence
    logic.

--- day15/day15.py
+++ day15/day15.py
@@ -24,26 +24,15 @@ def hash_value(value: str) -> int:
 
 
 def solve_part2(lines: list[str]) -> int:
-    total = 0
-
     sequence = lines[0].split(",")
     boxes = defaultdict(list)
 
     for seq in sequence:
         if "=" in seq:
-            op = "="
             label, focal = seq.split("=")
-        else:
-            op = "-"
-            label = seq.split("-")[0]
-        box_number = hash_value(label)
-        if boxes[box_number] is None:
-            lenses = []
-            boxes[box_number] = lenses
-        else:
+            box_number = hash_value(label)
             lenses = boxes[box_number]
 
-        if op == "=":
             for i, lens in enumerate(lenses):
                 if lens[0] == label:
                     lenses[i] = (label, focal)
@@ -51,22 +40,20 @@ def solve_part2(lines: list[str]) -> int:
             else:
                 lenses.append((label, focal))
         else:
+            label = seq[:-1]
+            box_number = hash_value(label)
+            lenses = boxes[box_number]
+
             for lens in lenses:
                 if lens[0] == label:
                     lenses.remove(lens)
+                    break
 
-    for box_number, lenses in boxes.items():
-        total += sum([focusing_power(box_number, lenses, i) for i in range(len(lenses))])
-
-    return total
+    return sum([sum([focusing_power(box, lens, i) for i, lens in enumerate(lenses)]) for box, lenses in boxes.items()])
 
 
-def focusing_power(box_number: str, lenses: list, index: int) -> int:
-    result = int(box_number) + 1
-    result *= (index + 1)
-    result *= int(lenses[index][1])
-
-    return result
+def focusing_power(box_number: int, lens: tuple, index: int) -> int:
+    return (box_number + 1) * (index + 1) * int(lens[1])
 
 
 if __name__ == '__main__':
